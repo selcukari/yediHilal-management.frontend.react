4a9299fd0f6c27f86785f5ea28746128acd129f4
selcuk.ari <selcukari51@gmail.com>
2025-08-18 11:47:43 +0300

    Country comp. add

---
+++ app/components/country.tsx
@@ -0,0 +1,14 @@
+import { Select } from '@mantine/core';
+
+export function Country() {
+  return (
+    <Select
+      label="Your favorite library"
+      placeholder="Pick value"
+      data={[{ value: 'react', label: 'React' }, { value: 'Angular', label: 'Vue' }, { value: 'svelte', label: 'Svelte'}]}
+      searchable
+      maxDropdownHeight={200}
+      nothingFoundMessage="Nothing found..."
+    />
+  );
+}
