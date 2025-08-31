adbe1d3b71cf78fe9ea2fb5577b3ae433a4f40f2
selcuk.ari <selcukari51@gmail.com>
2025-08-31 17:18:45 +0300

    refactor

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/member.tsx"), route("login", "routes/login.tsx"), route("users", "routes/user.tsx"), route("mails", "routes/mail.tsx"),
   route("sms", "routes/sms.tsx"), route("stocks", "routes/stock.tsx"), route("stockExpenses", "routes/stockUsedExpense.tsx"),
-  route("stockDeposits", "routes/stockUsedDeposit.tsx")
+  route("stockDeposits", "routes/stockUsedDeposit.tsx"), route("projects", "routes/project.tsx")
 ] satisfies RouteConfig;
 
