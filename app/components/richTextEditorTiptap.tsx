872a3057c044a3d00b1932ec5a3116ada0dcc8df
selcuk.ari <selcukari51@gmail.com>
2026-03-12 21:31:22 +0300

    review

--- app/components/richTextEditorTiptap.tsx
+++ app/components/richTextEditorTiptap.tsx
@@ -6,7 +6,6 @@ import { useEditor } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
 import type { UseFormReturnType } from '@mantine/form';
-
 import { RichTextEditor } from '@mantine/tiptap';
 
 interface RichTextEditorTiptapProps {
