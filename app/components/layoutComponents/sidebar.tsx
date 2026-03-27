cd9dad7dc1f1488ace1e9a9459215aae5e014164
selcuk.ari <selcukari51@gmail.com>
2026-03-27 15:45:14 +0300

    review

--- app/components/layoutComponents/sidebar.tsx
+++ app/components/layoutComponents/sidebar.tsx
@@ -76,7 +76,7 @@ const menuItems: MenuItem[] = [
       },
       { icon: IconClipboardList, label: 'Stok Yönetimi', key: 'stock', link: '/stocks',
         children: [{ label: 'Stok', key: 'stock-depo', link: '/stocks' },
-          { label: 'Talepler', key: 'stock-requestStocksManager', link: '/requestStocksManager' }, { label: 'Raflar', key: 'shelve-depo', link: '/shelves'},{ label: 'DepoLar', key: 'warehouse-depo', link: '/warehouses' }
+          { label: 'Talepler', key: 'stock-requestStocksManager', link: '/requestStocksManager' }, { label: 'Raflar', key: 'shelve-depo', link: '/shelves'},{ label: 'Depolar', key: 'warehouse-depo', link: '/warehouses' }
         ],
       },
     ]
