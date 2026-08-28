def63c87bfc65b52272cd39f55a4c84b85a569a4
selcuk.ari <selcukari51@gmail.com>
2026-08-28 22:00:57 +0300

    review

--- app/components/addOrEdit/programTypeSelect.tsx
+++ app/components/addOrEdit/programTypeSelect.tsx
@@ -2,6 +2,7 @@ import { Select } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
 import type { UseFormReturnType } from '@mantine/form';
 import { useProgramTypeService } from '../../services/programTypeService';
+
 interface ProgramTypeSelectProps {
   form: UseFormReturnType<any>;
   required?: boolean;
