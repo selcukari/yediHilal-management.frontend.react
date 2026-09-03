8b51b1c063320f0c3de2aa0f77ae053f883f100b
selcuk.ari <selcukari51@gmail.com>
2026-09-03 15:57:48 +0300

    review

--- app/components/project/projectEdit.tsx
+++ app/components/project/projectEdit.tsx
@@ -78,9 +78,7 @@ const ProjectEdit = forwardRef<ProjectEditDialogControllerRef, UserEditProps>(({
     });
   
     // Bileşen kapandığında (unmount) dinleyiciyi kaldırmazsanız memory leak oluşur ve mükerrer dinler.
-    return () => {
-      connection.off('ReceiveValueCreated');
-    };
+    return () => connection.off('ReceiveValueCreated');
   }, [connection]);
 
   const openDialog = (value: FormValues) => {
